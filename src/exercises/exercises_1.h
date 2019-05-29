ba07b6250e9e867f941ec4dda67a5e59c5ad6745
Terry Kaleas <terry.kaleas@gmail.com>
2019-05-29 15:36:21 -0700

    exercise update chapter 1

--- src/exercises/exercises_1.h
+++ src/exercises/exercises_1.h
@@ -3,6 +3,8 @@
 
 #include <iostream>
 
+#include "Sales_item.h"
+
 //C++ Primer Exercises 1.2
 static void exercise1_3() {
 	std::cout << "Hello World!" << std::endl;
@@ -60,4 +62,52 @@ static int exercise1_13() {
 
 	return sum;
 
+}
+
+static void exercise1_20() {
+	//read transactions from cin and print to cout
+	Sales_item item;
+	while (std::cin >> item) {
+		std::cout << item << std::endl;
+	}
+}
+
+
+static void exercise1_21() {
+	//print sum of 2 transactions
+	Sales_item item1, item2;
+	std::cin >> item1 >> item2;
+	if (item1.isbn() == item2.isbn()) {
+		std::cout << item1 + item2 << std::endl;
+	}
+}
+
+static void exercise1_22() {
+	//print sum of many transactions
+	Sales_item item, sum;
+	std::cin >> item;
+	sum = item;
+	std::cin >> item;
+	while(item.isbn() == sum.isbn()) {
+		sum += item;
+		std::cin >> item;
+	}
+}
+
+
+static void exercise1_23() {
+	//print sum of many transactions
+	Sales_item item, item1;
+	int num_transactions =0;
+	std::cin >> item1;
+	while (std::cin >> item) {
+		if (item1.isbn() == item.isbn()) {
+			num_transactions++;
+		}
+		else {
+			std::cout << item1 << num_transactions << std::endl;
+			item1 = item;
+			num_transactions = 0;
+		}
+	}
 }
